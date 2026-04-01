b8dfc178db5f57476a45f7838e59abfab2e6f836
Andre Brisco <91817010+abrisco@users.noreply.github.com>
2026-03-31 22:38:13 -0700

    Added rules_batch for entrypoint (#17)

--- powershell/private/entrypoint.bat
+++ powershell/private/entrypoint.bat
@@ -3,62 +3,22 @@
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-@REM Usage of rlocation function:
-@REM
-@REM        call :rlocation <runfile_path> <abs_path>
-@REM
-@REM        The rlocation function maps the given <runfile_path> to its absolute
-@REM        path and stores the result in a variable named <abs_path>. This
-@REM        function fails if the <runfile_path> doesn't exist in mainifest file.
-:: Start of rlocation
-goto :rlocation_end
-:rlocation
-if "%~2" equ "" (
-    echo>&2 ERROR: Expected two arguments for rlocation function.
-    exit 1
-)
-if exist "%RUNFILES_DIR%" (
-    set RUNFILES_MANIFEST_FILE=%RUNFILES_DIR%_manifest
-)
-if "%RUNFILES_MANIFEST_FILE%" equ "" (
-    set RUNFILES_MANIFEST_FILE=%~f0.runfiles\MANIFEST
-)
-if not exist "%RUNFILES_MANIFEST_FILE%" (
-    set RUNFILES_MANIFEST_FILE=%~f0.runfiles_manifest
-)
-set MF=%RUNFILES_MANIFEST_FILE:/=\%
-if not exist "%MF%" (
-    echo>&2 ERROR: Manifest file %MF% does not exist.
-    exit 1
-)
-set runfile_path=%~1
-for /F "tokens=2* usebackq" %%i in (`%SYSTEMROOT%\system32\findstr.exe /l /c:"!runfile_path! " "%MF%"`) do (
-    set abs_path=%%i
-)
-if "!abs_path!" equ "" (
-    echo>&2 ERROR: !runfile_path! not found in runfiles manifest
-    exit 1
-)
-set %~2=!abs_path!
-exit /b 0
-:rlocation_end
+@REM {RUNFILES_API}
 
+if not defined RUNFILES_DIR if not defined RUNFILES_MANIFEST_FILE (
+    if exist "%~f0.runfiles" (
+        set "RUNFILES_DIR=%~f0.runfiles"
+    ) else if exist "%~f0.runfiles_manifest" (
+        set "RUNFILES_MANIFEST_FILE=%~f0.runfiles_manifest"
+    ) else if exist "%~f0.exe.runfiles_manifest" (
+        set "RUNFILES_MANIFEST_FILE=%~f0.exe.runfiles_manifest"
+    ) else (
+        echo>&2 ERROR: cannot find runfiles
+        exit /b 1
+    )
+)
 
-@REM Function to replace forward slashes with backslashes.
-goto :slocation_end
-:slocation
-set "input=%~1"
-set "varName=%~2"
-set "output="
-
-@REM Replace forward slashes with backslashes
-set "output=%input:/=\%"
-
-@REM Assign the sanitized path to the specified variable
-set "%varName%=%output%"
-exit /b 0
-:slocation_end
-
+call :runfiles_export_envvars
 
 call :rlocation "{PWSH_INTERPRETER}" PWSH_INTERPRETER_PATH
 call :rlocation "{PROCESS_WRAPPER}" PROCESS_WRAPPER_PATH
